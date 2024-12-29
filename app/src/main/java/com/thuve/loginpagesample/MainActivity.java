e9d6575256ffd07e757726f52f1f4da2c044eaa4
thuve-codes <thuverakantharma@gmail.com>
2024-12-29 23:41:53 +0530

    Build some codes for json handle

--- app/src/main/java/com/thuve/loginpagesample/MainActivity.java
+++ app/src/main/java/com/thuve/loginpagesample/MainActivity.java
@@ -7,18 +7,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btnlogin,mainenbtn,regbtn,createbtn;
     EditText un,epass;
     TextView errortxt;
-
+    String jsonurl="https://raw.githubusercontent.com/thuve-codes/logindet-json/refs/heads/main/db.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -54,21 +49,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-            private boolean vaildun(String un) {
-                if (un.length() < 4) {
-                    errortxt.setText("Username must be greater than 4 characters");
-                    return false;
-                };
-
-                if (un.equals("admin")) {
-                    // errortxt.setText("Correct Credentials");
-                    return true;
-                }
-                return false;
-            };
 
 
-             void login() {
+
+            //--------------------------------
+            void login() {
                  setContentView(R.layout.activity_main);
                  btnlogin = findViewById(R.id.loginbtn);
                  un = findViewById(R.id.enterun);
@@ -79,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onClick(View v) {
-                        if (vaildun(un.getText().toString()) && epass.getText().toString().equals("admin")) {
+                        if (vailduser(un.getText().toString(), epass.getText().toString())) {
                             // errortxt.setText("");
                             setContentView(R.layout.mainpage);
 
@@ -92,6 +77,28 @@ public class MainActivity extends AppCompatActivity {
 
                 });
             }
+
+
+            //----------------------
+            //Json Handling
+
+
+            //----------------------
+
+            private boolean vailduser(String un,String pwd) {
+                if (un.length() < 4) {
+                    errortxt.setText("Username must be greater than 4 characters");
+                    return false;
+                };
+
+                //TODO: check user name is in json - for loop
+                if (un.equals("admin")) {
+                    //TODO: if found check given password is match with json
+                    // errortxt.setText("Correct Credentials");
+                    return true;
+                }
+                return false;
+            };
 
         });
     };
