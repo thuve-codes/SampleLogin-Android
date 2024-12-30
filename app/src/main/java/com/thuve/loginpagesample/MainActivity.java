9934c4093a3d5511f1be1d4aa7a2a64b155b5b7c
thuve-codes <thuverakantharma@gmail.com>
2024-12-30 12:56:54 +0530

    add working json env, now login sucess

--- app/src/main/java/com/thuve/loginpagesample/MainActivity.java
+++ app/src/main/java/com/thuve/loginpagesample/MainActivity.java
@@ -1,106 +1,135 @@
 package com.thuve.loginpagesample;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
-    Button btnlogin,mainenbtn,regbtn,createbtn;
-    EditText un,epass;
+    Button btnlogin, mainenbtn, regbtn, createbtn;
+    EditText un, epass;
     TextView errortxt;
-    String jsonurl="https://raw.githubusercontent.com/thuve-codes/logindet-json/refs/heads/main/db.json";
+
+    RequestQueue requestQueue; // Class-level variable for RequestQueue
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainen);
-        mainenbtn=findViewById(R.id.button);
 
+        // Initialize UI elements
+        mainenbtn = findViewById(R.id.button);
+        requestQueue = Volley.newRequestQueue(this); // Initialize RequestQueue
+
+        // Set up main button click
         mainenbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                login();
-
-
-                regbtn = findViewById(R.id.regbtn);
-                regbtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        setContentView(R.layout.signuppage);
-
-                        createbtn = findViewById(R.id.createid);
-                        createbtn.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Toast.makeText(MainActivity.this, "Reg Sucess, Login with Credentials", Toast.LENGTH_LONG).show();
-                                login();
-                            }
-                        });
-                    }
-                });
-
+                setupLoginView();
             }
-
-
-
-
-            //--------------------------------
-            void login() {
-                 setContentView(R.layout.activity_main);
-                 btnlogin = findViewById(R.id.loginbtn);
-                 un = findViewById(R.id.enterun);
-                 epass = findViewById(R.id.enterpassword);
-                 errortxt = findViewById(R.id.error);
-                btnlogin.setOnClickListener(new View.OnClickListener() {
-
-
-                    @Override
-                    public void onClick(View v) {
-                        if (vailduser(un.getText().toString(), epass.getText().toString())) {
-                            // errortxt.setText("");
-                            setContentView(R.layout.mainpage);
-
-                            Toast.makeText(MainActivity.this, "Sucess", Toast.LENGTH_LONG).show();
-
-                        } else {
-                            errortxt.setVisibility(v.VISIBLE);
-                        }
-                    };
-
-                });
-            }
-
-
-            //----------------------
-            //Json Handling
-
-
-            //----------------------
-
-            private boolean vailduser(String un,String pwd) {
-                if (un.length() < 4) {
-                    errortxt.setText("Username must be greater than 4 characters");
-                    return false;
-                };
-
-                //TODO: check user name is in json - for loop
-                if (un.equals("admin")) {
-                    //TODO: if found check given password is match with json
-                    // errortxt.setText("Correct Credentials");
-                    return true;
-                }
-                return false;
-            };
-
         });
-    };
+    }
 
-};
+    private void setupLoginView() {
+        setContentView(R.layout.activity_main);
+
+        // Initialize login UI elements
+        btnlogin = findViewById(R.id.loginbtn);
+        un = findViewById(R.id.enterun);
+        epass = findViewById(R.id.enterpassword);
+        errortxt = findViewById(R.id.error);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAPIRequest();
+            }
+        });
+
+        // Set up registration button
+        regbtn = findViewById(R.id.regbtn);
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setupSignupView();
+            }
+        });
+    }
+
+    private void setupSignupView() {
+        setContentView(R.layout.signuppage);
+
+        // Initialize signup UI elements
+        createbtn = findViewById(R.id.createid);
+        createbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Registration Successful, Login with Credentials", Toast.LENGTH_LONG).show();
+                setupLoginView();
+            }
+        });
+    }
+
+    private void setAPIRequest() {
+        String jsonUrl = "https://api.jsonserve.com/pnsoEE";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, jsonUrl, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("userdet");
+                            boolean userFound = false;
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject user = jsonArray.getJSONObject(i);
+                                String name = user.getString("username");
+                                String password = user.getString("password");
+
+                                if (name.equals(un.getText().toString())) {
+                                    userFound = true;
+                                    if (password.equals(epass.getText().toString())) {
+                                        setContentView(R.layout.mainpage);
+                                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                                        return;
+                                    } else {
+                                        errortxt.setText("Incorrect password!");
+                                        return;
+                                    }
+                                }
+                            }
+
+                            if (!userFound) {
+                                errortxt.setText("Username not found!");
+                            }
+                        } catch (JSONException e) {
+                            errortxt.setText("Error parsing response!");
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                errortxt.setText("Error connecting to server!");
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+}
